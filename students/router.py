5a04018f28f4f212b1d24e176f6606e2fe1a7e92
ann-e888 <asiedlarz4@gmail.com>
2023-05-28 23:20:12 +0200

    ruter change

--- students/router.py
+++ students/router.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from students.schema import StudentCreateSchema, StudentUpdateSchema
-from students.storage import get_students_data
+from students.schema import StudentCreateSchema, StudentUpdateSchema, Mark
+from students.storage import *
 
 router = APIRouter()
+
 @router.get("/")
 async def root():
     return []
@@ -12,12 +13,24 @@ student_list = get_students_data()
 
 @router.post("/", status_code = 200)
 async def create_student(student:StudentCreateSchema):
-    student_list.append(student)
-    return student 
+    if student.first_name is not None and student.last_name is not None:
+        student_list.append(student)
+        return student
+    raise HTTPException(
+        status_code= 406,
+        detail="Student must have name/surname"
+    )
+
 
 @router.put("/{student_id}")
 async def update_student(student_id: int, student: StudentUpdateSchema):
+    if student_id < 0 or student_id >= len(student_list):
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found."
+        )
     student_list[student_id] = student
+    return student 
 
 @router.get("/{student_id}")
 async def get_students(student_id):
@@ -27,6 +40,25 @@ async def get_students(student_id):
 async def get_student_list():
     return student_list
 
+@router.delete("/{student_id}")
+async def delete_student(student_id: int):
+    if student_id < len(student_list):
+        student_list.pop(student_id)
+        raise HTTPException(status_code=200, detail="Student deleted")
+    raise HTTPException(
+        status_code=404,
+        detail="Student can't be deleted because student doesn't exist"
+    )
 
+@router.post("/students/{student_id}/marks/{mark}")
+async def addmark(id: int, mark : Mark):
+    marks[id].append(mark.name)
+    raise HTTPException(status_code=200, detail="Grade added")
+
+@router.get("/{student_id}/marks")
+async def getmark(student_id:int):
+    if id < len(marks):
+        return marks[student_id]
+    raise HTTPException(status_code=404, detail="Student not found")
 
     
--- students/schema.py
+++ students/schema.py
@@ -10,7 +10,7 @@ class StudentUpdateSchema(BaseModel):
     first_name: str | None
     last_name: str | None
 
-class Mark(float, Enum):
+class Mark(str, Enum):
     BARDZO_DOBRY = 5.0
     DOBRY_PLUS = 4.5
     DOBRY = 4.0
